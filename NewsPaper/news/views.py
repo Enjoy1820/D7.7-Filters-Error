ca70af9e0c75b2610dc485019e683dd78f84d8cc
Enjoy1820 <165899162+Enjoy1820@users.noreply.github.com>
2024-07-13 14:02:34 +0300

    Add files via upload

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from django.views.generic import ListView, DeleteView
+
+from .filters import PostFilter
+from .models import Post
+
+
+class Postlist(ListView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['time_now'] = datetime.utcnow()
+    #     return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostDetail(DeleteView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
+
